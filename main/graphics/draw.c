bf26d5fe48b2b5960f72117eb0f3e88422e26648
AdarWa <adar.wasserman@gmail.com>
2026-04-08 22:14:26 +0300

    Optimized line and point algorithm

--- main/graphics/draw.c
+++ main/graphics/draw.c
@@ -8,10 +8,56 @@
 
 #include "graphic.h"
 
+static void draw_point(uint16_t x0, uint16_t y0) {
+    int r = BRUSH_DIAMETER / 2;
+    int x = r;
+    int y = 0;
+    int err = 0;
+
+    while (x >= y) {
+        for (int i = x0 - x; i <= x0 + x; i++) {
+            put_draw_buff(CLIP(i, 0, LCD_H_RES), CLIP(y0 + y, 0, LCD_V_RES), BRUSH_COLOR);
+            put_draw_buff(CLIP(i, 0, LCD_H_RES), CLIP(y0 - y, 0, LCD_V_RES), BRUSH_COLOR);
+        }
+        for (int i = x0 - y; i <= x0 + y; i++) {
+            put_draw_buff(CLIP(i, 0, LCD_H_RES), CLIP(y0 + x, 0, LCD_V_RES), BRUSH_COLOR);
+            put_draw_buff(CLIP(i, 0, LCD_H_RES), CLIP(y0 - x, 0, LCD_V_RES), BRUSH_COLOR);
+        }
+
+        if (err <= 0) {
+            y += 1;
+            err += 2 * y + 1;
+        }
+        if (err > 0) {
+            x -= 1;
+            err -= 2 * x + 1;
+        }
+    }
+}
+
+static void draw_line(int x0, int y0, int x1, int y1) {
+    int dx = abs(x1 - x0), sx = x0 < x1 ? 1 : -1;
+    int dy = -abs(y1 - y0), sy = y0 < y1 ? 1 : -1;
+    int err = dx + dy, e2;
+
+    while (1) {
+        draw_point(x0, y0);
+        if (x0 == x1 && y0 == y1) break;
+        e2 = 2 * err;
+        if (e2 >= dy) { err += dy; x0 += sx; }
+        if (e2 <= dx) { err += dx; y0 += sy; }
+    }
+}
+
 void handle_draw(esp_lcd_touch_point_data_t data) {
-    int x1 = CLIP(data.x - BRUSH_RADIUS/2, 0, LCD_H_RES);
-    int y1 = CLIP(data.y - BRUSH_RADIUS/2, 0, LCD_V_RES);
-    int x2 = CLIP(data.x + BRUSH_RADIUS/2, 0, LCD_H_RES);
-    int y2 = CLIP(data.y + BRUSH_RADIUS/2, 0, LCD_V_RES);
-    fill_draw_buff(x1, y1, x2, y2, BRUSH_COLOR);
+
+    static esp_lcd_touch_point_data_t last_point;
+    static bool first_touch = true;
+
+    if (first_touch) {
+        last_point = data;
+        first_touch = false;
+    }
+    draw_line(last_point.x, last_point.y, data.x, data.y);
+    last_point = data;
 }
--- main/graphics/draw.h
+++ main/graphics/draw.h
@@ -4,10 +4,14 @@
 
 #pragma once
 
+#include <math.h>
 #include <esp_lcd_touch.h>
 
 #define BRUSH_COLOR 0x07E0
-#define BRUSH_RADIUS 10
+#define BRUSH_DIAMETER 10
 #define CLIP(x, min, max) ((x) < (min) ? (min) : ((x) > (max) ? (max) : (x)))
+#define DEG_TO_RAD (M_PI / 180.0)
+#define MIN(a, b) (((a) < (b)) ? (a) : (b))
+#define MAX(a, b) (((a) > (b)) ? (a) : (b))
 
 void handle_draw(esp_lcd_touch_point_data_t data);
