bc701dffd045f4e5a45ab69d6126ac71d4f7f407
AdarWa <adar.wasserman@gmail.com>
2026-04-08 21:30:45 +0300

    Square brush impl

--- main/graphics/draw.c
+++ main/graphics/draw.c
@@ -9,5 +9,9 @@
 #include "graphic.h"
 
 void handle_draw(esp_lcd_touch_point_data_t data) {
-    put_draw_buff(data.x, data.y, BRUSH_COLOR);
+    int x1 = CLIP(data.x - BRUSH_RADIUS/2, 0, LCD_H_RES);
+    int y1 = CLIP(data.y - BRUSH_RADIUS/2, 0, LCD_V_RES);
+    int x2 = CLIP(data.x + BRUSH_RADIUS/2, 0, LCD_H_RES);
+    int y2 = CLIP(data.y + BRUSH_RADIUS/2, 0, LCD_V_RES);
+    fill_draw_buff(x1, y1, x2, y2, BRUSH_COLOR);
 }
--- main/graphics/draw.h
+++ main/graphics/draw.h
@@ -7,5 +7,7 @@
 #include <esp_lcd_touch.h>
 
 #define BRUSH_COLOR 0x07E0
+#define BRUSH_RADIUS 10
+#define CLIP(x, min, max) ((x) < (min) ? (min) : ((x) > (max) ? (max) : (x)))
 
 void handle_draw(esp_lcd_touch_point_data_t data);
