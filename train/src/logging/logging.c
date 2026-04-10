fc5ca02b20f887d01bd51ad1c03af4f8a2e0df71
AdarWa <adar.wasserman@gmail.com>
2026-04-10 16:17:46 +0300

    Add logging

---
+++ train/src/logging/logging.c
@@ -0,0 +1,27 @@
+//
+// Created by adarw on 4/10/26.
+//
+
+#include "logging.h"
+
+static const char* level_names[] = { "DEBUG", "INFO", "WARN", "ERROR", "FATAL" };
+
+void log_msg(LogLevel level, const char *tag, const char *fmt, ...) {
+    time_t now = time(NULL);
+    struct tm *t = localtime(&now);
+    char time_str[20];
+    strftime(time_str, sizeof(time_str), "%H:%M:%S", t);
+
+    printf("[%s] [%s] [%s] ", time_str, level_names[level], tag);
+
+    va_list args;
+    va_start(args, fmt);
+    vprintf(fmt, args);
+    va_end(args);
+    printf("\n");
+
+    if (level == L_FATAL) {
+        printf("!!! FATAL ERROR: Terminating program !!!\n");
+        exit(EXIT_FAILURE);
+    }
+}
---
+++ train/src/logging/logging.h
@@ -0,0 +1,26 @@
+//
+// Created by adarw on 4/10/26.
+//
+
+#pragma once
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <time.h>
+
+typedef enum {
+    L_DEBUG,
+    L_INFO,
+    L_WARN,
+    L_ERROR,
+    L_FATAL
+} LogLevel;
+
+void log_msg(LogLevel level, const char *tag, const char *fmt, ...);
+
+#define LOG_D(tag, ...) log_msg(L_DEBUG, tag, __VA_ARGS__)
+#define LOG_I(tag, ...) log_msg(L_INFO,  tag, __VA_ARGS__)
+#define LOG_W(tag, ...) log_msg(L_WARN,  tag, __VA_ARGS__)
+#define LOG_E(tag, ...) log_msg(L_ERROR, tag, __VA_ARGS__)
+#define LOG_F(tag, ...) log_msg(L_FATAL, tag, __VA_ARGS__)
