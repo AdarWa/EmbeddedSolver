f6c5543f91144af458122d13dd7901abccc30183
AdarWa <adar.wasserman@gmail.com>
2026-04-19 00:58:23 +0300

    Add batch seperator helper

---
+++ train/src/model/batch.h
@@ -0,0 +1,18 @@
+//
+// Created by adarw on 4/19/26.
+//
+
+#pragma once
+
+#include <stdlib.h>
+#include <dataset/img_handler/img_handler.h>
+
+typedef struct {
+    uint32_t cnt;
+    mnist_image_t* images;
+    uint8_t* labels;
+} batch_t;
+
+uint32_t get_batch_count(mnist_dataset_t* dataset, int batch_size);
+
+batch_t* get_batches(mnist_dataset_t* dataset, int batch_size);
