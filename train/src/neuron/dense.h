25cd8cc9dcbcb2ce17074d967c4f96247051cf7b
AdarWa <adar.wasserman@gmail.com>
2026-04-20 21:00:44 +0300

    Add backward pass for dense and dropout

--- train/src/neuron/dense.h
+++ train/src/neuron/dense.h
@@ -7,8 +7,13 @@
 
 typedef enum {
     RELU,
-    SOFTMAX
+    SOFTMAX,
+    IDENTITY
 } dense_activation_t;
 
 tensor_t* dense_layer_forward_pass(tensor_t* input, int count, tensor_t** weights, double* biases, dense_activation_t activation);
-tensor_t* dropout_forward_pass(const tensor_t* input, double drop_rate);
+tensor_t* dense_layer_backward_pass(tensor_t* d_out, tensor_t* input, tensor_t* forward_output,
+                                    tensor_t* weights, tensor_t* d_weights, double* d_biases,
+                                    int count, dense_activation_t activation);
+tensor_t* dropout_forward_pass(const tensor_t* input, double drop_rate, tensor_t** out_mask);
+tensor_t* dropout_backward_pass(const tensor_t* d_out, const tensor_t* mask, double drop_rate);
