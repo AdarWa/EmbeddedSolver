ea07c2f962ac1f358b6efdc9db4de747e393eab5
AdarWa <adar.wasserman@gmail.com>
2026-04-20 21:00:07 +0300

    Add backward pass for flatten

--- train/src/filters/flatten.c
+++ train/src/filters/flatten.c
@@ -44,3 +44,40 @@ tensor_t* flatten_tensor(tensor_t* input_tensor) {
 
     return output_tensor;
 }
+
+tensor_t* unflatten_tensor_backward(const tensor_t* d_upstream, const tensor_t* original_input) {
+    if (!d_upstream || !original_input) return NULL;
+
+    tensor_t* d_input = allocate_tensor(original_input->shape, original_input->ndim);
+    if (!d_input) return NULL;
+
+    int size = 1;
+    for (int i = 0; i < original_input->ndim; i++) {
+        size *= original_input->shape[i];
+    }
+
+    int* element_idx = NULL;
+    if (original_input->ndim > 0) {
+        element_idx = (int*)calloc(original_input->ndim, sizeof(int));
+    }
+
+    for (int i = 0; i < size; i++) {
+        // Get the gradient value from the 1D upstream tensor
+        int upstream_idx = i;
+        double grad_val = get_tensor_element(d_upstream, &upstream_idx);
+
+        set_tensor_element(d_input, element_idx, grad_val);
+
+        for (int j = original_input->ndim - 1; j >= 0; j--) {
+            element_idx[j]++;
+            if (element_idx[j] < original_input->shape[j]) {
+                break;
+            }
+            element_idx[j] = 0;
+        }
+    }
+
+    if (element_idx) free(element_idx);
+
+    return d_input;
+}
--- train/src/filters/flatten.h
+++ train/src/filters/flatten.h
@@ -7,3 +7,4 @@
 #include "../tensor/tensor.h"
 
 tensor_t* flatten_tensor(tensor_t* input_tensor);
+tensor_t* unflatten_tensor_backward(const tensor_t* d_upstream, const tensor_t* original_input);
