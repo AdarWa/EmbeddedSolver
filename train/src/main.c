f86bdb6431b4fa774255c934dbee68b6e1d6b630
AdarWa <adar.wasserman@gmail.com>
2026-04-20 19:44:01 +0300

    Add model train config

--- train/src/main.c
+++ train/src/main.c
@@ -5,8 +5,23 @@
 #include "dataset/img_handler/img_handler.h"
 #include "dataset/remote/remote.h"
 #include "logging/logging.h"
+#include "model/model.h"
 
 static const char* TAG = "main";
+static const model_train_config_t model_train_config = {
+    .epochs = 10,
+    .batch_size = 120,
+    .channel_cnt = 1,
+    .img_size = 28,
+    .filter_cnt_1 = 32,
+    .filter_cnt_2 = 64,
+    .kernel_size = 3,
+    .dense_cnt_1 = 128,
+    .dense_cnt_2 = 64,
+    .dense_cnt_3 = 10,
+    .dropout_rate_1 = 0.2,
+    .dropout_rate_2 = 0.2
+};
 
 int main() {
     if (!remote_all_data_exists()) {
@@ -26,7 +41,7 @@ int main() {
         LOG_F(TAG, "Found invalid dataset count!");
     }
 
-
+    train_model(model_train_config, &train_data);
 
     // Cleanup
     mnist_free_dataset(&train_data);
